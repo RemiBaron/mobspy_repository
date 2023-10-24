bc906a54df178a6545316f01b7ba08141ffdca32
RemiBaron <remi.baron@ens-paris-saclay.fr>
2023-10-24 17:54:53 +0200

    class_of_cts_meta_specie.py commented

--- mobspy-main/mobspy/modules/class_of_cts_meta_specie.py
+++ mobspy-main/mobspy/modules/class_of_cts_meta_specie.py
@@ -1,23 +1,42 @@
+"""
+    The class_of_cts_meta_specie.py model is responsible for defining the Cts_specie class.
+"""
 from mobspy.modules.meta_class import *
 from contextlib import contextmanager
 
 class Cts_specie(Species) :
     """
-        Class which inherits from Species. It is used to simplify the syntax of reactions inside a context.
+        Class which inherits from Species. It only has one object, Cts, which is defined at the end of this script.
+        It is used to simplify the syntax of reactions and count setting inside the body of a "with Cts.example_characteristic :" statement.
+        Is is compatible with event_condition and event_time methods of the Simulation class. 
+        It can be nested and used several time in the same with statement.
     """
 
     def cts_context_initiator(self,characteristic):
+        """
+            This sets the Cts context in all meta-species.
+
+            :params characteristic: (str) characteristic of the Cts specie in this specific context. Characteristic to be added.
+        """
         self.add_characteristic(characteristic)
-        # Set context in all meta-species
         Species.update_cts_context(self.get_characteristics())
 
     def cts_context_finish(self,characteristic):
+        """
+            This removes the Cts context in all meta-species.
+
+            :params characteristic: (str) characteristic of the Cts specie in this specific context. Characteristic to be removed.
+        """
         self.remove_characteristic(characteristic)
-        # Set context in all meta-species
         Species.update_cts_context(self.get_characteristics())
 
     @contextmanager
     def __getattr__(self, characteristic):
+        """
+            Called inside a with statement. It manages the characteristic in the Cts_context.
+
+            :params characteristic: (str) characteristic of the Cts specie in this specific context. Characteristic to be managed.
+        """
         try:
             self.cts_context_initiator(characteristic)
             yield 0
@@ -56,6 +75,5 @@ class Cts_specie(Species) :
 
    
 
-    
 __SCts = Cts_specie('Context_MetaSpecies')
 Cts = __SCts
